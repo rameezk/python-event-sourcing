07bdbe07dd4474de3fd561b21906ccf3e552bb3e
Rameez Khan <rameezkhan.sa@gmail.com>
2020-02-19 16:23:50 +0200

    feat: add additional test for my sanity

--- order_aggregate_test.py
+++ order_aggregate_test.py
@@ -13,3 +13,17 @@ class OrderAggregateTest(unittest.TestCase):
         order = Order.create(user_id=2)
         order.set_status("shipped")
         self.assertEqual(order.changes, [OrderCreated(2), StatusChanged("shipped")])
+
+    def test_should_have_the_correct_events(self):
+        order = Order.create(user_id=3)
+        order.set_status("paid")
+        order.set_status("confirmed")
+        self.assertEqual(
+            order.changes,
+            [
+                OrderCreated(user_id=3),
+                StatusChanged("paid"),
+                StatusChanged("confirmed"),
+            ],
+        )
+        self.assertEqual(order.status, "confirmed")
